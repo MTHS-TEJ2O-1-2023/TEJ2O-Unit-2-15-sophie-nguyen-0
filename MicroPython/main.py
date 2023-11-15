57d2a6e647b2dc55c7524cb20926f2b0f28148ae
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-11-15 18:29:44 +0000

    py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,29 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: sophie
+Created on: Nov 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+# variables
+x_counter = 0
+y_counter = 0
+
+# setup
+display.clear()
+display.show(Image.HEART)
+sleep(1000)
+
+# loop
+while True:
+    if button_a.is_pressed():
+        # setup
+        display.clear()
+        y_counter = 0
+
+        while y_counter = 0 || 4:
+
+            while x_counter = 
+
