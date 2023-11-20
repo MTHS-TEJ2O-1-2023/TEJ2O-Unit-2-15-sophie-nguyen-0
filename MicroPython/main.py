6d0c870256c2f3b8d1d4ffb70b1bc076a338ca2b
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-11-20 18:27:28 +0000

    py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,16 +1,15 @@
 """
 Created by: sophie
 Created on: Nov 2023
-This module is a Micro:bit MicroPython program makes LED light go around the edge of Microbit
+This module is a Micro:bit MicroPython program that moves a pixel around the edge.
 """
 
 from microbit import *
 
 
 # variables
-x_counter = 0
-y_counter = 0
-loop_counter = 0
+y_coordinate = 0
+x_coordinate = 0
 
 # setup
 display.clear()
@@ -22,13 +21,46 @@ while True:
     if button_a.is_pressed():
         # setup
         display.clear()
-        y_counter = 0
-        x_counter = 0
 
-        while loop_counter <= 2:
-            while x_counter < 5:
+        # reset variables
+        y_coordinate = 0
+        x_coordinate = 0
+
+        # loop that moves pixel down
+        while y_coordinate <= 3:
+            # loop that moves pixel right
+            while x_coordinate <= 3:
                 sleep(500)
-                display.set_pixel(x_counter, y_counter, 9)
-                x_counter += 1
 
-            while y_counter < 5:
+                display.set_pixel(x_coordinate, y_coordinate, 9)
+                sleep(500)
+                display.clear()
+
+                x_coordinate += 1
+            sleep(500)
+            display.clear()
+            display.set_pixel(x_coordinate, y_coordinate, 9)
+            y_coordinate += 1
+        # delete pixel after it reaches (4, 3)
+        display.clear()
+
+        # loop that moves pixel up
+        while y_coordinate >= 0:
+            # loop that moves pixel left
+            while x_coordinate >= 1:
+                sleep(500)
+
+                display.set_pixel(x_coordinate, y_coordinate, 9)
+                sleep(500)
+                display.clear()
+
+                x_coordinate -= 1
+
+            sleep(500)
+            display.clear()
+            display.set_pixel(x_coordinate, y_coordinate, 9)
+            y_coordinate -= 1
+        # when full turn is completed
+        sleep(500)
+        display.clear()
+        display.show(Image.HEART)
